02b51bd68942f1f51a66c6afba5d7b4ff16cd225
王宇华 <suikalala@gmail.com>
2019-05-30 15:59:13 +0800

    add params.go : modify your params here

---
+++ server/params.go
@@ -0,0 +1,13 @@
+package main
+import "github.com/aws/aws-sdk-go/aws"
+
+var (
+	InstanceID       *string = aws.String("")
+	BotToken         string  = ""
+	UpInstanceType   string  = ""
+	DownInstanceType string  = ""
+	ChatID           int     = 123456
+	Region           *string = aws.String("")
+	AWSID            string  = ""
+	AWSSecret        string  = ""
+)
