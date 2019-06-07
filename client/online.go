2f02bc1721e7221630ff479605e390a24f1f0f4d
王宇华 <suikalala@gmail.com>
2019-06-07 20:03:42 +0800

    online.go: no need to call AWS API to stop instance

--- client/online.go
+++ client/online.go
@@ -4,7 +4,6 @@ import (
 	"os/exec"
 	"fmt"
 	"log"
-	"net/rpc"
 	"strconv"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"time"
@@ -13,47 +12,12 @@ import (
 var ChatID           int     = 12345678
 var BotToken         string  = ""
 
-func NewTeleBot() (*tgbotapi.BotAPI, error){
-	// Init Telegram Bot
-	bot, err := tgbotapi.NewBotAPI(BotToken)
-	if err != nil {
-		log.Fatal(err)
-		return &tgbotapi.BotAPI{}, err
-	}
-	bot.Debug = true
-	return bot, nil
-}
-
-func StopCommand() {
-	client, err := rpc.Dial("tcp", "3.112.232.91:42586")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Init Telegram Bot
-	bot, _ := NewTeleBot()
-
-	var line []byte
-	var reply bool
-
-	err = client.Call("Listener.StopInstance", line, &reply)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Call("Listener.DescribeInstance", line, &reply)
-	if err != nil {
-		log.Fatal(err)
-	}
-	msg := tgbotapi.NewMessage(int64(ChatID), "Successfully Stop Instance")
-	bot.Send(msg)
-	fmt.Println("Success to Stop Instance")
-}
 
 func main() {
+	bot, _ := tgbotapi.NewBotAPI(BotToken)
 	cmd := exec.Command("/bin/bash", "-c", `who | wc -l`)
 	count := 1
-	for count != 0 {
+	for count == 0 {
 		out, err := cmd.Output()
 		if err != nil {
 			log.Fatal(err)
@@ -67,5 +31,8 @@ func main() {
 		fmt.Println(count)
 		time.Sleep(2 * time.Minute)
 	}
-	StopCommand()
+	msg := "shut down"
+	bot.Send(tgbotapi.NewMessage(int64(ChatID), msg))
+	cmd = exec.Command("/bin/bash", "-c", "sudo poweroff")
+	cmd.Run()
 }
